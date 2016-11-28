d3a7d732db2bbd597dd9db053df62f976264c059
Lukasz Machura <lukasz.machura@alfa1.smcebi.us.edu.pl>
2016-11-28 09:57:37 +0000

    1. histogram przez slownik 2. sys do pobierania pliku do analizy
    TODO:
        wywalic liczenie znakow

--- readTxt.py
+++ readTxt.py
@@ -1,18 +1,24 @@
-File = open('file.txt').read()
+import sys
 
 def scanFile(text):
-    wordText = '';
-    charCounter = 0;
     
-    for i in text:
-        
-        if ( i == ' ') or ( i == '\n' ):
-            print wordText, charCounter
-            wordText = '';
-            charCounter = 0;
-            continue;
-        
-        wordText += i;
-        charCounter += 1;
+    histogram = {}
+    split_text = text.split(" ")
+    
+    for wyraz in split_text:
+        if len(wyraz) in histogram:
+            histogram[len(wyraz)] += 1
+        else:
+            histogram[len(wyraz)] = 1
+    
+    return histogram
 
-scanFile( File );
+        
+if len(sys.argv) == 1:
+    file_name = 'file.txt'
+else:
+    file_name = sys.argv[1]
+
+with open(file_name) as f:
+    File = f.read()
+    print scanFile(File);
