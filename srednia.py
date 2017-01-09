10beb9bd1909a264ae5b5835eb100e67731c1a4f
LM <lukasz.machura@us.edu.pl>
2017-01-09 10:55:46 +0100

    test travis

---
+++ srednia.py
@@ -0,0 +1,5 @@
+def srednia(L):
+    assert isinstance(L, (list, tuple))
+    return float(sum(L)) / len(L)
+
+
---
+++ test.py
@@ -0,0 +1,11 @@
+from srednia import srednia
+
+L = [1, 1, 1, 1, 1]
+assert srednia(L) == 1.0
+
+L = [10, 20 , 30 , 40 ,50]
+assert srednia(L) == 30.0
+
+#L = ['a', 2, 2]
+#assert srednia(L) == 2.0
+
